2aecda3f0348e3c70f11d40f8451847804fce713
İlke Aşağıçayır <ilke.asagicayir@gmail.com>
2023-03-11 01:04:17 +0300

    Duplicate Numbers

---
+++ java5/src/java5/RepetitiveNumbers.java
@@ -0,0 +1,41 @@
+package java5;
+
+import java.util.Arrays;
+
+public class RepetitiveNumbers {
+	
+	static boolean isFind(int[] arr, int value) {
+		for(int i : arr) {
+			if(i == value) {
+				return true;
+			}
+		}
+		
+		return false;
+	}
+	
+	public static void main(String[] args) {
+		int[] list = {9,9,3,7,8,5,1,5,4,4,1,2,3};
+		int[] duplicate = new int[list.length];
+		int startIndex = 0;
+		for(int i = 0; i<list.length;i++) {
+			for(int j = 0; j<list.length;j++) {
+				if( i!=j && list[i] == list[j]) {
+					if(!isFind(duplicate, list[i])) {
+						duplicate[startIndex++] = list[i];
+					}
+					break;
+				}
+			}
+		}
+		//System.out.println(Arrays.toString(duplicate));
+		
+		for(int i : duplicate) {
+			if(i != 0) {
+				if(i % 2 ==0){
+					System.out.print("Sayı dizisindeki tekrar eden çift sayılar : "+i);
+				}
+			}
+		}
+	}
+}
